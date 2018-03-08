97fd71055cdc73b85cb5ebda3c844a677faf5d0e
Marco Fontani <MFONTANI@cpan.org>
2018-03-08 10:16:03 +0100

    add test for TX with cache => 2 and rm cache before -C run

--- benchmark.pl
+++ benchmark.pl
@@ -28,6 +28,8 @@ const my $DEFAULT_ITERATIONS => 500;
 const my $DEFAULT_RUNTIME    => 1; # seconds
 const my $TT_DIR             => './tt_templates';
 const my $TX_DIR             => './tx_templates';
+const my $TT_CACHE_DIR       => './.tt_cache';
+const my $TX_CACHE_DIR       => './.tx_cache';
 const my $RESULTS_DIR        => './results';
 const my $RX_NUMBER          => qr!\A\d+(?:[.]\d+)?\z!xms;
 
@@ -36,9 +38,10 @@ mkdir $RESULTS_DIR
 
 # Global Options
 my ($FORCE)     = grep { $_ eq '-f' }       @ARGV; @ARGV = grep { $_ ne '-f' } @ARGV;
+my ($CACHE)     = grep { $_ eq '-C' }       @ARGV; @ARGV = grep { $_ ne '-C' } @ARGV;
 my ($DUMBBENCH) = grep { $_ eq '-D' }       @ARGV; @ARGV = grep { $_ ne '-D' } @ARGV;
 my ($LIST)      = grep { $_ eq 'list' }     @ARGV; @ARGV = grep { $_ ne 'list' } @ARGV;
-my ($NARROW)    = grep { $_ eq '-n' }       @ARGV; @ARGV = grep { $_ ne '-n' } @ARGV;
+my ($WIDE)      = grep { $_ eq '-w' }       @ARGV; @ARGV = grep { $_ ne '-w' } @ARGV;
 my ($RUNTIME)   = grep { $_ =~ $RX_NUMBER } @ARGV; @ARGV = grep { $_ !~ $RX_NUMBER } @ARGV;
 $RUNTIME //= $DEFAULT_RUNTIME;
 
@@ -51,7 +54,7 @@ my $TT = Template->new(
         # URI::XSEscape speeds up TT's own version
         uri => \&URI::XSEscape::uri_escape_utf8,
     },
-    COMPILE_DIR  => "$FindBin::RealBin/.tt_cache/",
+    COMPILE_DIR  => "$FindBin::RealBin/$TT_CACHE_DIR",
     COMPILE_EXT  => '.ttc2',
 );
 
@@ -61,6 +64,7 @@ $TX = Text::Xslate->new(
     type        => 'html',
     input_layer => ':utf8',    # No need for :set bomb
     path        => [ $TX_DIR ],
+    cache       => 1,          # the default cache level (check freshness every time)
     module      => [
         # Use similar variable methods to TT2
         'Text::Xslate::Bridge::TT2Like',
@@ -72,12 +76,45 @@ $TX = Text::Xslate->new(
             return Text::Xslate::mark_raw( $TX->render("$_[0]", $TX->current_vars) );
         },
     },
-    cache_dir => './.tx_cache/',
+    cache_dir => $TX_CACHE_DIR,
 );
 
+my $TXC; # to allow the functions to reference the templating system
+$TXC = Text::Xslate->new(
+    verbose     => 1,          # warn about non-trivial stuff
+    type        => 'html',
+    input_layer => ':utf8',    # No need for :set bomb
+    path        => [ $TX_DIR ],
+    cache       => 2,          # use cache; do not check for freshness
+    module      => [
+        # Use similar variable methods to TT2
+        'Text::Xslate::Bridge::TT2Like',
+    ],
+    function => {
+        # Xslate's uri filter is a lot faster than this
+        # uri => \&URI::XSEscape::uri_escape_utf8,
+        runtime_include => sub {
+            return Text::Xslate::mark_raw( $TX->render("$_[0]", $TX->current_vars) );
+        },
+    },
+    cache_dir => $TX_CACHE_DIR,
+);
+
+# Always reset cache directory before each run, to ensure TXC cache => 2
+# setting will not wreak havoc with updated / uncached templates; it shouldn't,
+# as TX is done first, so the cache for any updated template should be already
+# updated by the time TXC happens, but better safe than sorry.
+if ($CACHE) {
+    warn "Purging $TX_CACHE_DIR...\n";
+    require File::Path;
+    warn "Removed ", File::Path::rmtree($TX_CACHE_DIR), " files from TX cache.\n";
+}
+
 {
-    my @cols = $NARROW ? ('Function',                          'TT/s',                          'TX/s', "±TX/TT\n&num")
-             :           ('Function', 'TT done', 'TT seconds', 'TT/s', 'TX done', 'TX seconds', 'TX/s', "±TX/TT\n&num");
+    my @cols = $WIDE ?  ('Function', 'TT done', 'TT seconds', 'TT/s', 'TX done', 'TX seconds', 'TX/s', "±TX/TT\n&num")
+             :          ('Function',                          'TT/s',                          'TX/s', "±TX/TT\n&num");
+    push @cols, 'TXC/s', pop @cols, "±TXC/TT\n&num", "±TXC/TX\n&num"
+        if $CACHE;
     my $table       = Text::Table->new(@cols);
     my @jsons       = reverse glob './data/*.json';
     my %wants_tests = map { $_ => 1 } @ARGV;
@@ -124,6 +161,9 @@ sub sanity_check {
     my $tt_data = _benchmark_one('TT', $base, \&tt_exec, $TT, $tt_file, $json);
     my $tx_data = _benchmark_one('TX', $base, \&tx_exec, $TX, $tx_file, $json);
 
+    _benchmark_one('TXC', $base, \&tx_exec, $TXC, $tx_file, $json)
+        if $CACHE;
+
     if ($tt_data ne $tx_data) {
         warn "$base output differs!\nTT: \Q$tt_data\E\nTX: \Q$tx_data\E\n";
         path("./tt.out")->spew_utf8($tt_data);
@@ -140,15 +180,23 @@ sub benchmark {
     my ($tt_file, $tx_file) = files_for($base);
     sanity_check($base, $tt_file, $tx_file, $json);
 
-    my $tt_data = _benchmark_all('TT', $base, \&tt_exec, $TT, $tt_file, $json);
-    my $tx_data = _benchmark_all('TX', $base, \&tx_exec, $TX, $tx_file, $json);
+    my $txc_data;
+    my $tt_data = _benchmark_all('TT',  $base, \&tt_exec, $TT,  $tt_file, $json);
+    my $tx_data = _benchmark_all('TX',  $base, \&tx_exec, $TX,  $tx_file, $json);
+    $txc_data   = _benchmark_all('TXC', $base, \&tx_exec, $TXC, $tx_file, $json)
+        if $CACHE;
 
     my @cols = ("${RUNTIME}s $base");
-    push @cols, ($tt_data->{iterate}, (sprintf '%.2f', $tt_data->{done})) if !$NARROW;
+    push @cols, ($tt_data->{iterate}, (sprintf '%.2f', $tt_data->{done})) if $WIDE;
     push @cols, sprintf '%.2f', $tt_data->{per_sec};
-    push @cols, ($tx_data->{iterate}, (sprintf '%.2f', $tx_data->{done})) if !$NARROW;
+    push @cols, ($tx_data->{iterate}, (sprintf '%.2f', $tx_data->{done})) if $WIDE;
     push @cols, sprintf '%.2f', $tx_data->{per_sec};
-    push @cols, sprintf '%+.2f%%', $tx_data->{per_sec} * 100 / $tt_data->{per_sec};
+    push @cols, sprintf '%.2f', $txc_data->{per_sec} if $CACHE;
+    push @cols, sprintf '%+.2f%%', $tx_data->{per_sec}  * 100 / $tt_data->{per_sec};
+    push @cols, sprintf '%+.2f%%', $txc_data->{per_sec} * 100 / $tt_data->{per_sec}
+        if $CACHE;
+    push @cols, sprintf '%+.2f%%', $txc_data->{per_sec} * 100 / $tx_data->{per_sec}
+        if $CACHE;
     $table->add(@cols);
 }
 
